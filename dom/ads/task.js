05ec6ec11dc60af5d085e8c7e8e87045967f0d1f
JulieSoboleva <ju.soboll@gmail.com>
2024-01-25 10:21:03 +0300

    Task 2 done

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,7 +1,9 @@
 const texts = document.querySelectorAll('.rotator__case');
 
 let index = 0;
-const intId = setInterval(() => {
+let delay = texts[index].getAttribute('data-speed');
+
+function main() {
     for (let i = 0; i < texts.length; i++) {
         if (i == index) {
             texts[i].classList.add('rotator__case_active');
@@ -13,4 +15,23 @@ const intId = setInterval(() => {
     if (index == texts.length) {
         index = 0;
     }
-}, 1000);
+
+    delay = texts[index].getAttribute('data-speed');
+    texts[index].style.color = texts[index].getAttribute('data-color');
+    timer.setInterval(delay);
+}
+
+function Timer(callback, interval) {
+    this.callback = callback;
+    this.timerId = null;
+    this.setInterval(interval);
+}
+
+Timer.prototype.setInterval = function (val) {
+    if (this.timerId !== null) {
+        clearInterval(this.timerId);
+    }
+    this.timerId = setInterval(this.callback, val);
+}
+
+let timer = new Timer(main, delay);
